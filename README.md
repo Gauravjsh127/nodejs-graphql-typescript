6d020755f969a36846c7301b332e4297a6ee15d1
Joshi Gaurav <joshigaurav@dvg-qkj6970hw.bcgcloud.com>
2023-05-13 11:57:51 +0200

    srv updated

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # nodejs-graphql-typescript
 Nodejs typescript
-.
+
+
+# run locally
+
+npm run start
+
+
+
+
+
+There are a few ways to set up a GraphQL API in Node.js. These options may include: 
+
+Apollo Server
+Hasura
+Prisma
+PostGraphile
+Alternatively, you can build your own server with custom resolvers and a schema definition. While there are pros and cons for each method, we’ll use PostGraphile in this tutorial. We’ve made that choice because it provides an instant GraphQL API based on the DB schema.
--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,6 @@
+
+import postgraphile from 'postgraphile'
+
 import express from 'express'
 import { AppDataSource } from './data-source'
 import { Product } from './entity/Product'
@@ -11,9 +14,20 @@ import { Warehouse } from './entity/Warehouse'
  * This is our main entry point of our Express server.
  * All the routes in our API are going to be here.
  **/ 
+const pgUser = 'postgres:password'
+
+/**
+* This is our main entry point of our Express server.
+* All the routes in our API are going to be here.
+**/
 const App = () => {
   const app = express()
   app.use(express.json())
+  app.use(postgraphile(`postgresql://${pgUser}@localhost/catalog_db`, 'public', {
+    watchPg: true,
+    graphiql: true,
+    enhanceGraphiql: true,
+  }))
 
   app.get('/api/v1/hello', async (req, res, next) => {
     res.send('success')
